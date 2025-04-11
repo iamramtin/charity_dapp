e7e4dffe59b60ce5540d5a2bba132f97dd1fc34c
ulieth <uliana.eth@proton.me>
2025-04-11 23:57:50 +0300

    started working with solana blinks: imported the necessary functions from @solana/actions

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,12 @@
+import { ActionError, ActionGetResponse, ActionPostRequest, ActionPostResponse, createActionHeaders, createPostResponse } from "@solana/actions";
+
+
+
+
+const headers = createActionHeaders();
+
+
+
 export async function GET(request: Request) {
   return new Response('Hello, from API!')
 }
