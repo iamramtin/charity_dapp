7aa5b8683735b8151107093fd86c34648e8300f1
ulieth <uliana.eth@proton.me>
2025-04-13 20:28:43 +0300

    default values for errors handling

--- src/app/api/actions/const.ts
+++ src/app/api/actions/const.ts
@@ -1,3 +1,4 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(""); // devnet wallet
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey("9MipEJLetsngpXJuyCLsSu3qTJrHQ6E6W1rZ1GrG68am"); // This program devnet address
+export const DEFAULT_SOL_AMOUNT: number = 0.1; // 0.1 SOL
